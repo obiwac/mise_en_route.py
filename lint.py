1a7dbbb6653844b788e00f56bde38298916b43d2
obiwac <obiwac@gmail.com>
2021-09-20 19:34:53 +0200

    Fixed linter options

--- lint.py
+++ lint.py
@@ -2,7 +2,13 @@ import pylint.lint as linter
 
 print("Starting linter ...")
 
-results = linter.Run(["./src"], do_exit = False)
+LINTER_OPTIONS = [
+	"--disable", "C0114", # disable 'missing-module-docstring'
+	"--disable", "C0103", # disable 'invalid-name' (pylint thinks 'sum_of_squares' is a constant when, in reality, it isn't)
+	"--disable", "W0311", # TIL some dumbasses unironically prefer spaces to tabs 🤮
+]
+
+results = linter.Run(["./src"] + LINTER_OPTIONS, do_exit = False)
 score = results.linter.stats["global_note"]
 
 if score < 10:
